065857c1391d809a439ae10b3465a10b2adb2684
TotoB12 <totobeliard@outlook.com>
2023-08-14 10:48:13 -0700

    updated file with 0

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-haha
+import os
+import time
+
+while True:
+    with open('file.txt', 'w') as f:
+        f.write('0')
+    os.system('git add .')
+    os.system('git commit -m "updated file with 0"')
+    print("updated file with 0")
+    os.system('git push')
+    print("pushed")
+    time.sleep(1)
+
+    with open('file.txt', 'w') as f:
+        f.write('1')
+    os.system('git add .')
+    os.system('git commit -m "updated file with 1"')
+    print("updated file with 1")
+    os.system('git push')
+    print("pushed")
+    time.sleep(1)
