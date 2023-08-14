93de9f8a72e7b42fefbd54c522676fb6ac1ff419
TotoB12 <totobeliard@outlook.com>
2023-08-14 10:54:15 -0700

    updated file with 1

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import os
-import time
 
 while True:
     with open('file.txt', 'w') as f:
         f.write('0')
-    os.system('git add .')
-    os.system('git commit -m "updated file with 0"')
+    os.system('git commit -am "updated file with 0"')
     print("updated file with 0")
     os.system('git push')
     print("pushed")
-    # time.sleep(1)
 
     with open('file.txt', 'w') as f:
         f.write('1')
-    os.system('git add .')
-    os.system('git commit -m "updated file with 1"')
+    os.system('git commit -am "updated file with 1"')
     print("updated file with 1")
     os.system('git push')
     print("pushed")
-    # time.sleep(1)
